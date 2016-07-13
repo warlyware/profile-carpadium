d5ab8e1a958749a92f62f0772a7b32befbf0f087
warlyware <danward@gmail.com>
2016-07-12 20:49:50 -0400

    Replace connect with browserSync in gulp process, base new project off early commit from profile-jonward

--- gulpfile.js
+++ gulpfile.js
@@ -3,49 +3,44 @@
 var gulp = require('gulp');
 var config = require('./gulp/config.json');
 var sass = require('gulp-sass');
-var connect = require('gulp-connect');
 var concatCss = require('gulp-concat-css');
-
-
-// watch files for changes and reload
+var browserSync = require('browser-sync').create();
 
 gulp.task('styles', function() {
     return gulp.src(config.paths.src.styles)
         .pipe(sass().on('error', sass.logError))
         .pipe(concatCss('style.css'))
-        .pipe(gulp.dest('build'))
-        .pipe(connect.reload());
+        .pipe(gulp.dest('build'));
 });
 
 gulp.task('copy:html', function() {
   return gulp.src(config.paths.src.html)
-    .pipe(gulp.dest('build'))
-    .pipe(connect.reload());
+    .pipe(gulp.dest('build'));
 });
 
 gulp.task('copy:js', function() {
   return gulp.src(config.paths.src.js)
-    .pipe(gulp.dest('build'))
-    .pipe(connect.reload());
+    .pipe(gulp.dest('build'));
 });
 
 gulp.task('copy:node_modules', function() {
   return gulp.src(config.paths.node_modules)
-    .pipe(gulp.dest('build/node_modules'))
-    .pipe(connect.reload());
+    .pipe(gulp.dest('build/node_modules'));
 });
 
-gulp.task('serve', function() {
-    connect.server({
-        root: 'build',
-        livereload: true
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./build"
+        }
     });
 });
 
 gulp.task('watch', function() {
-    gulp.watch(config.paths.src.html, ['copy:html']);
-    gulp.watch(config.paths.src.js, ['copy:js']);
-    gulp.watch(config.paths.src.styles, ['styles']);
+    gulp.watch(config.paths.src.html, ['copy:html']).on("change", browserSync.reload);;
+    gulp.watch(config.paths.src.js, ['copy:js']).on("change", browserSync.reload);;
+    gulp.watch(config.paths.src.styles, ['styles']).on("change", browserSync.reload);;
 });
 
-gulp.task('default', ['styles', 'copy:html', 'copy:js', 'copy:node_modules', 'serve', 'watch']);
+
+gulp.task('default', ['styles', 'copy:html', 'copy:js', 'copy:node_modules', 'browser-sync', 'watch']);
