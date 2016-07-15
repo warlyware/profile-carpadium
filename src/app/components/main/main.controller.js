41f751f15443a7fac4703ca8c0a648c15a4d6274
warlyware <danward@gmail.com>
2016-07-14 22:32:21 -0400

    Add .onload wrapper for animations

--- src/app/components/main/main.controller.js
+++ src/app/components/main/main.controller.js
@@ -1,19 +1,22 @@
 angular.module('profile-carpadium')
-    .controller('MainController', [function() {
+    .controller('MainController', ['$window', function($window) {
         console.log('Main Ctrl online');
 
         angular.element(document).ready(function () {
-            var carpadiumWord = document.getElementById('carpadium-word');
-            var southEastTour = document.getElementById('south-east-tour');
-            var albumCover = document.getElementById('album-cover');
+            $window.onload = function() {
+                var carpadiumWord = document.getElementById('carpadium-word');
+                var southEastTour = document.getElementById('south-east-tour');
+                var albumCover = document.getElementById('album-cover');
 
-            TweenLite.to(carpadiumWord, 1.5, {autoAlpha:1});
-            TweenLite.to(carpadiumWord, 1.5, {autoAlpha:0, delay: 1.5});
+                TweenLite.to(carpadiumWord, 1.5, {autoAlpha:1});
+                TweenLite.to(carpadiumWord, 1.5, {autoAlpha:0, delay: 1.5});
 
-            TweenLite.to(southEastTour, 1.5, {autoAlpha:1, delay: 3});
-            TweenLite.to(southEastTour, 1.5, {autoAlpha:0, delay: 4.5});
+                TweenLite.to(southEastTour, 1.5, {autoAlpha:1, delay: 3});
+                TweenLite.to(southEastTour, 1.5, {autoAlpha:0, delay: 4.5});
 
-            TweenLite.to(albumCover, 3, {autoAlpha:1, delay: 6});
+                TweenLite.to(albumCover, 3, {autoAlpha:1, delay: 6});
+
+            }
         });
 
     }]);
