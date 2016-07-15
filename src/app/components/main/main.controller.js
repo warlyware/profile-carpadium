e15f8b3ff1fdb0504bb97fefc6679a4807711cf5
warlyware <danward@gmail.com>
2016-07-14 22:35:32 -0400

    Try window.onload with angular doc ready check

--- src/app/components/main/main.controller.js
+++ src/app/components/main/main.controller.js
@@ -2,21 +2,20 @@ angular.module('profile-carpadium')
     .controller('MainController', ['$window', function($window) {
         console.log('Main Ctrl online');
 
-        angular.element(document).ready(function () {
-            $window.onload = function() {
-                var carpadiumWord = document.getElementById('carpadium-word');
-                var southEastTour = document.getElementById('south-east-tour');
-                var albumCover = document.getElementById('album-cover');
 
-                TweenLite.to(carpadiumWord, 1.5, {autoAlpha:1});
-                TweenLite.to(carpadiumWord, 1.5, {autoAlpha:0, delay: 1.5});
+        $window.onload = function() {
+            var carpadiumWord = document.getElementById('carpadium-word');
+            var southEastTour = document.getElementById('south-east-tour');
+            var albumCover = document.getElementById('album-cover');
 
-                TweenLite.to(southEastTour, 1.5, {autoAlpha:1, delay: 3});
-                TweenLite.to(southEastTour, 1.5, {autoAlpha:0, delay: 4.5});
+            TweenLite.to(carpadiumWord, 1.5, {autoAlpha:1});
+            TweenLite.to(carpadiumWord, 1.5, {autoAlpha:0, delay: 1.5});
 
-                TweenLite.to(albumCover, 3, {autoAlpha:1, delay: 6});
+            TweenLite.to(southEastTour, 1.5, {autoAlpha:1, delay: 3});
+            TweenLite.to(southEastTour, 1.5, {autoAlpha:0, delay: 4.5});
 
-            }
-        });
+            TweenLite.to(albumCover, 3, {autoAlpha:1, delay: 6});
+
+        }
 
     }]);
